7994c8181376574528c40261b99039c0d9c029a5
Byczax <byczqq136@gmail.com>
2022-03-01 12:53:33 +0000

    add lab6

---
+++ lab6/zad8.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# Systemy operacyjne 2 – laboratorium nr 6 – semestr letni 2020/2021
+#
+# Celem zajęć jest zapoznanie się z możliwościami narzędzia awk.
+#
+# Tradycyjnie nie przywiązujemy zbyt dużej wagi do środowiska roboczego.
+# Zakładamy, że format i układ danych w podanych plikach nie ulega zmianie,
+# ale same wartości, inne niż podane wprost w treści zadań, mogą ulec zmianie,
+# a przygotowane zadania wciąż powinny działać poprawnie (robić to, co trzeba).
+#
+# Wszystkie chwyty dozwolone, ale w wyniku ma powstać tylko to, o czym jest
+# mowa w treści zadania – nie generujemy żadnych dodatkowych plików pośrednich.
+# Większość rozwiązań powinno sprowadzić się do jednego, zmyślnego wywołania
+# programu `awk` z odpowiednimi argumentami.
+# 
+#
+
+#
+# Zadanie 8.
+#
+# Plik `dodatkowe/lipsum.txt` zawiera przykładowy tekst z generatora
+# http://lipsum.pl/. Używając programu awk, proszę przygotować narzędzie,
+# które rozbije tekst w podanym pliku wejściowym w taki sposób, aby każda
+# linia miała mniej niż 80 znaków (przy czym staramy się zmakymalizować
+# liczbę znaków w każdej linii jak to możliwe). Jako wynik programu wyświetlić
+# tekst po przekształceniu.
+#
+
+awk '{
+    for(i=1;i<NF;i++){
+        sum+=length($i);
+        if(sum>=80){
+            print ""; 
+            sum=length($i);
+            }
+        else{
+            if (i!=1){printf " "};
+            sum+=1
+        }
+        printf $i 
+        }
+    }
+    END {
+        print " " $NF }' dodatkowe/lipsum.txt
+        # total+=$i;
+        # if(total>=80){
+            # print "\n";
+            # total=0};
+        # print $i}' 
+        
