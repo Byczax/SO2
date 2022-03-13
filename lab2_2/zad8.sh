6f2c2d8dd165ed6cf681ef50b1d3dfa44ac24e75
Byczax <byczqq136@gmail.com>
2022-03-13 22:10:56 +0000

    lab2 new version
    Signed-off-by: Byczax <byczqq136@gmail.com>

---
+++ lab2_2/zad8.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Systemy operacyjne 2 – laboratorium nr 2
+#
+# Celem zajęć jest nabranie doświadczenia w podstawowej pracy z powłoką Bash,
+# w szczególności w nawigowaniu po drzewie katalogów i sprawdzaniu uprawnień.
+# Proszę unikać wykorzystywania narzędzia `find` w ramach bieżących zajęć.
+#
+# Nie przywiązujemy wagi do środowiska roboczego – zakładamy, że jego pliki,
+# inne niż te podane wprost w treści zadań, mogą ulec zmianie, a przygotowane
+# rozwiązania nadal powinny działać poprawnie (robić to, o czym zadanie mówi).
+#
+# Wszystkie chwyty dozwolone, ale ostatecznie w wyniku ma powstać tylko to,
+# o czym mowa w treści zadania – tworzone samodzielnie ewentualne tymczasowe
+# pliki pomocnicze należy usunąć.
+#
+
+#
+# Zadanie 8.
+# Wyszukać w katalogu `dane/` i jego bezpośrednich podkatalogach (jeden poziom
+# wgłąb) wszystkie pliki zwyczajne (nie katalogi!), które są w systemie plików
+# oznaczone jako wykonywalne. Wyświetlić ścieżki do wszystkich znalezionych
+# plików względem katalogu `dane/`. Każdą ścieżkę wyświetlić w osobnej linii.
+#
+
+for file in dane/*;do
+    if ! test -f "$file";then
+        for deep_file in "$file"/*;do
+            if test -f "$deep_file";then
+                if test -x "$deep_file";then
+                    echo "$deep_file" | sed 's|dane/||'
+                fi
+            fi
+        done
+    else
+        if test -x "$file";then
+            echo "$file" | sed 's|dane/||'
+        fi
+    fi
+done
